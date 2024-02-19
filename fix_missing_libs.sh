7d5ffc38970942a3fd6e84a328daecbb5b16f6cc
DyDaDo13 <dydado13@gmail.com>
2024-02-19 15:12:49 +0100

    da

--- fix_missing_libs.sh
+++ fix_missing_libs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install libx11-dev -y && sudo apt-get install libxext-dev -y
+sudo apt-get update -y && sudo apt-get install libx11-dev -y && sudo apt-get install libxext-dev -y && sudo apt-geet install libbsd-dev
